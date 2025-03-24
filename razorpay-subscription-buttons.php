25efa68160227dc11251f688bb76fb642f2afe5f
Razorpay <abdul.sharief@razorpay.com>
2025-03-24 15:56:00 +0530

    PO-328 version bump 1.0.5

--- razorpay-subscription-buttons.php
+++ razorpay-subscription-buttons.php
@@ -4,7 +4,7 @@
  * Plugin Name: Razorpay Subscription Button Elementor
  * Plugin URI:  https://github.com/razorpay/subscription-button-elementor-plugin
  * Description: Razorpay Subscription Button Elementor
- * Version:     1.0.4
+ * Version:     1.0.5
  * Author:      Razorpay
  * Author URI:  https://razorpay.com
  */
